359d55e1bf44ba8b4851d7370dc90577cc4f518e
mukeshpanch14 <mukesh.panch14@gmail.com>
2019-03-19 09:59:05 +0000

    1

---
+++ Vivek_Brajesh/test.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+
+%matplot inline
+plt.plot( [1,2,3,4], [4,3,2,1] )
+plt.show()
--- WebScrap/tes.py
+++ WebScrap/tes.py
@@ -1,4 +1,11 @@
-from urllib.request import urlopen
-textPage = urlopen(
-             "http://www.pythonscraping.com/pages/warandpeace/chapter1-ru.txt")
-print(str(textPage.read(),'utf-8'))
+tup = 'panch', 'mukesh','track','luck','appplic'
+print(tup)
+
+def tupprint(tuple):
+    out=()
+    i=0
+    for tup in tuple:
+    if i%2!=0:
+        out=out
+    
+tupprint(tup)
